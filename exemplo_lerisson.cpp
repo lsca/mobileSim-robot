e61f87a79eefc9f838022ef1f57806c93ff1d6c2
osboxes.org <osboxes@localhost.localdomain>
2017-09-26 17:39:40 -0300

    add do meu arquivo e do de lerisson

---
+++ exemplo_lerisson.cpp
@@ -0,0 +1,67 @@
+#include "Aria.h"
+int main(int argc, char** argv)
+{
+//Inicio obrigatorio do Aria
+ Aria::init();
+//Objeto conexão(simpleConnector) com simulador ou robô
+ArSimpleConnector simpleConnector(&argc,argv);
+//Objeto robo(robot)
+ ArRobot robot;
+//Testa argumentos
+ if(!simpleConnector.parseArgs() || argc>1)
+{
+simpleConnector.logOptions();
+exit(1);
+}
+//Testa conexao com simulador ou robo
+if(!simpleConnector.connectRobot(&robot))
+{
+printf("Nao conseguiu conectar ao robo\n");
+Aria::exit(1);
+}
+//Cria objeto sonar
+ArSonarDevice sonar;
+//Conecta o sonar ao robo
+ robot.addRangeDevice(&sonar);
+//Inicializa variaveis de velocidade(vel), distancia(dist) e leiturade sonar(valorsonar)
+ int vel,valorsonar,dist;
+
+//Inicializa robo
+ robot.runAsync(true);
+
+//Habilita motores
+robot.comInt(ArCommands::ENABLE,1);
+printf("Entre com a distancia minima da parede: ");
+scanf("%d",&dist);
+printf ("Entre com a velocidade: ");
+scanf("%d",&vel);
+
+//Determina a velocidade máxima de translacao do robo
+robot.setTransVelMax(vel);
+//Faz com que o robo se movimente com velocidade vel
+robot.setVel(vel);
+ //Realiza medida do menor valor apresentado pelos sonares frontais
+valorsonar=robot.getClosestSonarRange(-90,90);
+while(true){
+//Realiza medidas do sonar ate que se torne menor do que dist
+while(valorsonar>dist)
+valorsonar=robot.getClosestSonarRange(-90,90);
+//Pára o robo
+robot.stop();
+//Faz com que o robo retorne 500mm
+robot.move(-500);
+//Aguarda realizacao do movimento
+while(!robot.isMoveDone());
+//Faz com que o robô gire 45 graus
+robot.setDeltaHeading(45);
+//Aguarda termino de giro
+while(!robot.isHeadingDone());
+
+//Faz com que o robo se movimente com velocidade vel
+robot.setVel(vel);
+//Realiza menor medida dos sonares frontais
+valorsonar=robot.getClosestSonarRange(-90,90);
+robot.moveTo(ArPose(robot.getX(),robot.getY(),robot.getTh()))
+}
+Aria::exit(0);
+}
---
+++ teste_Raz.cpp
@@ -0,0 +1,84 @@
+#include "Aria.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  Aria::init();
+  ArArgumentParser parser(&argc, argv);
+  parser.loadDefaultArguments();
+  ArRobot robot;
+
+  // Connect to the robot, get some initial data from it such as type and name,
+  // and then load parameter files for this robot.
+  ArRobotConnector robotConnector(&parser, &robot);
+  if(!robotConnector.connectRobot())
+  {
+    ArLog::log(ArLog::Terse, "simpleConnect: Could not connect to the robot.");
+    if(parser.checkHelpAndWarnUnparsed())
+    {
+        // -help not given
+        Aria::logOptions();
+        Aria::exit(1);
+    }
+  }
+
+  if (!Aria::parseArgs() || !parser.checkHelpAndWarnUnparsed())
+  {
+    Aria::logOptions();
+    Aria::exit(1);
+  }
+
+  ArLog::log(ArLog::Normal, "simpleConnect: Connected to robot.");
+
+  double xInicial_Mapa,yInial_Mapa,angulo,xFinal_Mapa,yFinal_Mapa;
+  cout << "O programa comeca agora!!" << endl;
+  cin >> xInicial_Mapa;
+  cin >> yInial_Mapa;
+  cin >> angulo;
+  cin >> xFinal_Mapa;
+  cin >> yFinal_Mapa;
+  printf("X inicial: %.1f \nY inicial: %.1f\nGrau: %.1f \nX Final: %.1f \nY Final: %.1f\n",xInicial_Mapa,yInial_Mapa,angulo,xFinal_Mapa,yFinal_Mapa);
+
+  robot.runAsync(true); //Roda no modo assicrono
+
+  robot.lock(); // Bloqueia o robô durante a montagem
+  robot.comInt(ArCommands::ENABLE, 1); //Liga os motores
+  robot.setVel(0); //Seta a velocidade de translação para 200 mm/s
+  robot.setRotVel(20); //Seta a velocidade rotacional para 20 graus/s
+  robot.setVel2(200,200); //Seta a velocidade das rodas para 200 mm/s
+  robot.setHeading(0); //30 graus relativo a posição inicial
+  robot.setDeltaHeading(0); //60 graus relativo a orientação atual
+  robot.unlock(); //Desbloqueia o robô
+
+  int count = 1;
+  char ent;
+  while (true) {
+    robot.move(50); //Move 200 mm/s pra frente
+    if(count%10000 == 0){
+    cout << "Posicao X em relacao a ele: " << robot.getX() << endl;
+    cout << "Posicao Y em relacao a ele: " << robot.getY() << endl << endl;
+    }
+    count++;
+  }
+
+  /*robot.enableMotors();
+  robot.runAsync(true);
+  robot.lock();
+  bool soc = robot.hasStateOfCharge();
+
+  float battv = 0.0;
+  if(soc)
+    battv = robot.getStateOfCharge();
+  else
+    battv = robot.getBatteryVoltage();
+
+  ArLog::log(ArLog::Normal, "simpleConnect: Pose=(%.2f,%.2f,%.2f), Trans.  Vel=%.2f, Battery=%.2f%c",
+    robot.getX(), robot.getY(), robot.getTh(), robot.getVel(), battv, soc?'%':'V');
+  robot.unlock();
+
+  robot.stopRunning();
+  robot.waitForRunExit();*/
+  Aria::exit(0);
+  return 0;
+}
